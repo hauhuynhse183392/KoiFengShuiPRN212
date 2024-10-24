0ed3fc2183426a51f307d4dd2fbdf1d11519cfb6
huynqse183261 <huynqse183261@fpt.edu.vn>
2024-10-24 11:29:20 +0700

    Manager User

---
+++ KoiFengShuiSystemPRN212/FengShuiKoi_Services/IMemberService.cs
@@ -0,0 +1,19 @@
+﻿using FengShuiKoi_BO;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FengShuiKoi_Services
+{
+    public interface IMemberService
+    {
+        Task<Member> GetMemberByUserID(string userid);
+        Task<List<Member>> GetMembers();
+        Task<bool> AddMember(Member member);
+        Task<bool> DeleteAccount(string id);
+        Task<bool> UpdateMember(Member updatedMember);
+        Task<Dictionary<string, int>> GetUsersByAgeGroup();
+     
+    }
+}
